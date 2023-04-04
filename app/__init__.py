40318ca6e10a4f48974c4aa49ed64d3a3a0b0dcd
edgar <johnnyavila2021@u.northwestern.edu>
2023-04-04 05:08:43 -0400

    add for loop to jinja feed template to iterate throught tweets

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,21 @@
 # !!START
-from flask import Flask
+from flask import (Flask, render_template)
 from .config import Config
+from .tweets import tweets
+import random
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
 # !!END
+
+
+@app.route('/')
+def hello():
+    tweet = random.choice(list(tweets))
+    return render_template('index.html', tweet=tweet)
+
+
+@app.route('/feed')
+def feed():
+    return render_template('feed.html', tweets=tweets)
